59414e6cd243451f6f0bc33e50ac1900a8156ced
saitheinhan <48027541+saitheinhan@users.noreply.github.com>
2020-05-14 20:58:41 +0800

    Update video post table

--- model/user.go
+++ model/user.go
@@ -23,27 +23,39 @@ type User struct {
 }
 
 //category table
-type Category struct{
-	Categoryid uint `gorm:"primary_key"`
-	Categoryname string
+type Category struct {
+	Categoryid    uint `gorm:"primary_key"`
+	Categoryname  string
 	Categoryorder int
 }
 
 //user post table
-type Userpost struct{
+type Userpost struct {
 	Userpostid uint `gorm:"primary_key"`
-	Userid int
+	Userid     int
 	Createdate time.Time
 }
 
 //moment post tabel
-type Momentpost struct{
-	Momentid uint `gorm:"primary_key"`
-	Userid int
+type Momentpost struct {
+	Momentid   uint `gorm:"primary_key"`
+	Userid     int
 	Userpostid int
 	Categoryid int
-	Caption string
-	Image string
-	Likecount int
+	Caption    string
+	Image      string
+	Likecount  int
 	Createdate time.Time
+}
+
+//video post table
+type Videopost struct{
+	Videoid uint `gorm:"primary_key"`
+	Videourl string
+	Caption string
+	Categoryid int
+	Userpostid int
+	Userid int
+	Viewcount int
+	Likecount int
 }
